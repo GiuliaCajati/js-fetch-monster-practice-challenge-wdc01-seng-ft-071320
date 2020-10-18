9f972d0a66c6998df213c718edf950b731d3115e
aysikh <amy.sikhammountry@gmail.com>
2020-10-18 10:30:12 -0400

    done

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   monsterForm()
 })
 
+// `${URL}/?_limit=10`
+
 function fetchMonsters() {
-  fetch(URL)
+  fetch(`${URL}?_limit=50`) // show first 50
   .then(rsp => rsp.json())
   .then(monsterArray => {
     monsterArray.forEach(monster => renderMonster(monster))
@@ -28,19 +30,49 @@ function renderMonster(monster){
 
 }
 
-function monsterForm(monster){
+function monsterForm(){
   let createMonsterForm = document.getElementById("create-monster")
   let form = document.createElement("form")
   form.setAttribute = "monster-form"
   let nameEl = document.createElement("input")
-  nameEl.setAttribute = ("id", "name")
-  nameEl.setAttribute = ("placeholder", "name")
+  nameEl.id = "name"
+  
   let ageEl = document.createElement("input")
+  ageEl.id = "age"
 
   let descriptionEl = document.createElement("input")
+  descriptionEl.id = "description"
+
   let formButton = document.createElement("button")
   formButton.innerText = "Create Monster"
+  formButton.addEventListener("click", () => {
+    event.preventDefault();
+    addMonster();
+    console.log("does this work");
+  })
 
   createMonsterForm.append(form)
   form.append(nameEl, ageEl, descriptionEl, formButton)
+
+}
+
+function addMonster(){
+debugger
+  let newName = document.querySelector("input#name").value
+  let newAge = document.querySelector("input#age").value
+  let newDescription = document.querySelector("input#description").value
+
+  let requestPackage = {};
+  requestPackage.method = "POST";
+  requestPackage.headers = {'Content-Type': 'application/json'};
+  requestPackage.body = JSON.stringify({
+    "name": newName,
+    "age": newAge, 
+    "description": newDescription
+    });
+
+    fetch(URL, requestPackage)
+    .then(rsp => rsp.json())
+    .then(newMonster => renderMonster(newMonster));
+
 }
